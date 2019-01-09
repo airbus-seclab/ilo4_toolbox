f6f039305713d8385dc23a309c813ac4c894e984
Alexandre Gazet <alexandre.gazet@airbus.com>
2019-01-09 13:21:07 +0100

    ilo4_extract - support firmware based on GUID instead of magic

--- scripts/iLO4/ilo4_extract.py
+++ scripts/iLO4/ilo4_extract.py
@@ -18,7 +18,7 @@ END_CERT = "-----END CERTIFICATE-----\n"
 
 IMG_LIST = ["elf", "kernel_main", "kernel_recovery"]
 
-HPIMAGE_HDR_SIZE = 0x4B8
+HPIMAGE_HDR_SIZE = 0x4A0
 BOOTLOADER_HDR_SIZE = 0x440
 IMG_HDR_SIZE = 0x440
 
@@ -33,7 +33,7 @@ outdir = sys.argv[2]
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
-with open(filename,"rb") as fff:
+with open(filename, "rb") as fff:
     data = fff.read()
 
 offsets_map = OrderedDict()
@@ -46,7 +46,7 @@ if not data.startswith(BEGIN_SIGN):
     print "[-] Bad file format\n    No \"%s\" signature" % BEGIN_SIGN.rstrip()
     sys.exit(1)
 
-off=data.find(END_SIGN)+len(END_SIGN)
+off = data.find(END_SIGN) + len(END_SIGN)
 
 # discard for now
 data = data[off:]
@@ -55,7 +55,7 @@ global_offset = off
 
 cert_num = 0
 while data.startswith(BEGIN_CERT):
-    off=data.find(END_CERT)+len(END_CERT)
+    off = data.find(END_CERT) + len(END_CERT)
     cert_data = data[:off]
     data = data[off:]
     offsets_map["HP_CERT%d" % cert_num] = global_offset
@@ -74,21 +74,54 @@ if not data.startswith("HPIMAGE"):
     sys.exit(1)
 
 hpimage_header = data[:HPIMAGE_HDR_SIZE]
-
 data = data[HPIMAGE_HDR_SIZE:]
+
 offsets_map["HPIMAGE_HDR"] = global_offset
 global_offset += HPIMAGE_HDR_SIZE
+
+print "[+] iLO HPIMAGE header :"
 
 with open(outdir + "/hpimage.hdr", "wb") as fff:
     fff.write(hpimage_header)
 
-guid = hpimage_header[0xc:0x1c]
+img_hdr = HpImageHeader.from_buffer_copy(hpimage_header)
+img_hdr.dump()
 
-if not data.startswith("iLO3") and not data.startswith("iLO4"):
-    print "[-] Bad file format"
-    sys.exit(1)
 
-# get signature: should be ilO3 or ilO4
+#------------------------------------------------------------------------------
+# extract target info
+
+targetListsize = unpack_from("<L", data)[0]
+
+print "\n[+] iLO target list: %x element(s)" % (targetListsize)
+
+data = data[4:]
+global_offset += 4
+
+for i in range(targetListsize):
+    raw = data[:0x10]
+    dev = ""
+    id = uuid.UUID(raw.encode("hex"))
+    if id in TARGETS:
+        dev = TARGETS[id]
+
+    print "    target 0x%x (%s)" % (i, dev)
+    print hexdump(raw)
+
+    if dev == "":
+        print "[x] unknown target"
+        sys.exit(0)
+
+    data = data[0x10:]
+    global_offset += 0x10
+
+data = data[4:]
+global_offset += 4
+
+
+#------------------------------------------------------------------------------
+# get signature: should be iLO3, iLO4 or iLO5
+
 ilo_sign = data[:4]
 ilo_bootloader_header = data[:BOOTLOADER_HDR_SIZE]
 ilo_bootloader_footer = data[-0x40:]
@@ -108,6 +141,7 @@ bootloader_header.dump()
 with open(outdir + "/bootloader.sig", "wb") as fff:
     fff.write(bootloader_header.to_str(bootloader_header.signature))
 
+
 #------------------------------------------------------------------------------
 # extract Bootloader footer and cryptographic parameters
 
@@ -122,7 +156,7 @@ print "\ntotal size:    0x%08x" % total_size
 print "payload size:  0x%08x" % len(data)
 print "kernel offset: 0x%08x\n" % bootloader_footer.kernel_offset
 
-offsets_map["BOOTLOADER"] = global_offset + total_size -bootloader_footer.kernel_offset - BOOTLOADER_HDR_SIZE
+offsets_map["BOOTLOADER"] = global_offset + total_size - bootloader_footer.kernel_offset - BOOTLOADER_HDR_SIZE
 ilo_bootloader = data[-bootloader_footer.kernel_offset:-BOOTLOADER_HDR_SIZE]
 
 with open(outdir + "/bootloader.bin", "wb") as fff:
@@ -130,7 +164,7 @@ with open(outdir + "/bootloader.bin", "wb") as fff:
 
 data = data[:total_size-BOOTLOADER_HDR_SIZE]
 
-ilo_crypto_params = data[ len(data)-((~bootloader_footer.sig_offset + 1) & 0xFFFF): len(data)-0x40]
+ilo_crypto_params = data[len(data)-((~bootloader_footer.sig_offset + 1) & 0xFFFF): len(data)-0x40]
 
 with open(outdir + "/sign_params.raw", "wb") as fff:
     fff.write(ilo_crypto_params)
@@ -142,7 +176,7 @@ crypto_params.dump()
 #------------------------------------------------------------------------------
 # extract images
 
-ilo_num=0
+ilo_num = 0
 
 off = data.find(ilo_sign)
 
@@ -161,10 +195,10 @@ while off >= 0:
     ilo_header = data[:IMG_HDR_SIZE]
     data = data[IMG_HDR_SIZE:]
 
-    with open(outdir + "/%s.hdr"% IMG_LIST[ilo_num], "wb") as fff:
+    with open(outdir + "/%s.hdr" % IMG_LIST[ilo_num], "wb") as fff:
         fff.write(ilo_header)
 
-    print "[+] iLO Header %d: %s" % (ilo_num,ilo_header[:0x1a])
+    print "[+] iLO Header %d: %s" % (ilo_num, ilo_header[:0x1a])
 
     img_header = ImgHeader.from_buffer_copy(ilo_header)
     img_header.dump()
@@ -183,12 +217,12 @@ while off >= 0:
     offsets_map["%s" % IMG_LIST[ilo_num].upper()] = global_offset
     global_offset += payload_size
 
-    psz, = unpack_from("<L",data1)
+    psz, = unpack_from("<L", data1)
     data1 = data1[4:]
     assert(psz == payload_size-4)
     assert(psz == len(data1))
 
-    window=['\0']*0x1000
+    window = ['\0'] * 0x1000
     wchar = 0
 
     with open(outdir + "/%s.raw" % IMG_LIST[ilo_num], "wb") as fff:
@@ -196,7 +230,7 @@ while off >= 0:
 
     print "[+] Decompressing"
 
-    output_size = decompress_all(data1,outdir + "/%s.bin" % IMG_LIST[ilo_num])
+    output_size = decompress_all(data1, outdir + "/%s.bin" % IMG_LIST[ilo_num])
     print "    decompressed size : 0x%08x\n" % (output_size)
 
     print "[+] Extracted %s.bin" % IMG_LIST[ilo_num]
--- scripts/iLO4/ilo4lib.py
+++ scripts/iLO4/ilo4lib.py
@@ -1,6 +1,39 @@
 import os
 from ctypes import *
 from struct import pack
+import uuid
+
+
+DEVICES = {
+    uuid.UUID("9d7b312fe3c9764dbff6b9d0d085a952"): "ILO",
+    uuid.UUID("2e8d14aa096e3e45bc6f63baa5f5ccc4"): "SYSTEM_ROM",
+    uuid.UUID("916b239911c283429ca97423f25687f3"): "CUSTOM_ROM",
+    uuid.UUID("9a43adb1d19dc141a4962da9313f1f07"): "CPLD",
+    uuid.UUID("3bad180a84cb0c479050cafb33371a14"): "CARBONDALE",
+    uuid.UUID("8aa2489e6c5819458405a04f84e27f0f"): "PIC",
+    uuid.UUID("90aa533689703a45899c792827a50d67"): "NVME_BP_PIC",
+    uuid.UUID("7760b86b75021446aae186618e8b1c27"): "POWER_SUPPLY",
+    uuid.UUID("dffc32e2cbbc5347a99bf6b11c6eb074"): "EEPROM_I2C",
+    uuid.UUID("18077fda4c441c49b9bfb5a9ccc5e6e8"): "FILES",
+    uuid.UUID("0c4c1027c53a91498afbd1f3cd166fb4"): "LANGUAGE_PACK",
+    uuid.UUID("a8d1685fab9795408c68bc3e1125268b"): "ILO_MOONSHOT",
+    uuid.UUID("8384790bfcabcc4c914e26c4fb948cff"): "CPLD_MOONSHOT"
+}
+
+TARGETS = {
+    uuid.UUID("2932ecaecc69d843bd0e61dc3406f71b"): "ILO_4",
+    uuid.UUID("0000000000000000000000000000ffff"): "SERVER_ID",
+    uuid.UUID("00000000000000000000000001FFFFFF"): "BIOS",
+    uuid.UUID("00000000000000000000000001ffffff"): "BOOTBLOCK_0",
+    uuid.UUID("00000000000000000000000001ffffff"): "BOOTBLOCK 1",
+    uuid.UUID("0000000000000000000000000000cdff"): "CARBONDALE 1",
+    uuid.UUID("00000000000000000000000000504dff"): "POWER PIC",
+    uuid.UUID("000000000000000000000000ffffffff"): "NMVE_BP_PIC",
+    uuid.UUID("4cb0f50e84b9984295f04b3fffffffff"): "OEM_DATA",
+    uuid.UUID("ffffffffffff000000000cf38db966ea"): "PS1",
+    uuid.UUID("ffffffffffff000000000cf38db966ea"): "PS2"
+}
+
 
 def hexdump(src, length=16):
     FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
@@ -32,6 +65,58 @@ class SignatureParams(LittleEndianStructure):
         print hexdump(self.to_str(self.exponent))
 
 
+class HpImageHeader(LittleEndianStructure):
+
+    _fields_ = [
+        ("img_magic", c_char * 0x8),
+        ("major", c_byte),
+        ("minor", c_byte),
+        ("field_A", c_ushort),
+        ("device_id", c_byte * 0x10),
+        ("field_1C", c_uint),
+        ("field_20", c_uint),
+        ("field_24", c_uint),
+        ("field_28", c_uint),
+        ("field_2C", c_uint),
+        ("field_30", c_uint),
+        ("field_34", c_uint),
+        ("field_38", c_uint),
+        ("field_3C", c_uint),
+        ("version", c_char * 0x20),
+        ("name", c_char * 0x40),
+        ("gap", c_byte * 0x400),
+    ]
+
+    def to_str(self, byte_array):
+        return str(bytearray(byte_array))
+
+    def dump(self):
+        print "  > img_magic          : %s" % self.to_str(self.img_magic)
+        print "  > version major      : 0x%x" % self.major
+        print "  > version minor      : 0x%x" % self.minor
+        print "  > field_A            : 0x%02x" % self.field_A
+
+        dev = ""
+        dev_id = uuid.UUID(self.to_str(self.device_id).encode("hex"))
+        if dev_id in DEVICES:
+            dev = DEVICES[dev_id]
+
+        print "  > device id          : %s" % dev
+        print hexdump(self.to_str(self.device_id))
+        print "  > field_1C            : 0x%x" % self.field_1C
+        print "  > field_20            : 0x%x" % self.field_20
+        print "  > field_24            : 0x%x" % self.field_24
+        print "  > field_28            : 0x%x" % self.field_28
+        print "  > field_2C            : 0x%x" % self.field_2C
+        print "  > field_30            : 0x%x" % self.field_30
+        print "  > field_34            : 0x%x" % self.field_34
+        print "  > field_38            : 0x%x" % self.field_38
+        print "  > field_3C            : 0x%x" % self.field_3C
+        print "  > version             : %s" % self.to_str(self.version)
+        print "  > name                : %s" % self.to_str(self.name)
+        print "  > gap"
+
+
 class BootloaderHeader(LittleEndianStructure):
 
     _fields_ = [
@@ -55,7 +140,7 @@ class BootloaderHeader(LittleEndianStructure):
 
     def dump(self):
         print "  > magic              : %s" % self.to_str(self.ilO_magic)
-        print "  > build_version      : %s" % self.build_version.split("\x1A")[0]
+        print "  > build_version      : %s" % self.build_version.split("\x1A")[0].strip()
         print "  > type               : 0x%02x" % self.type
         print "  > compression_type   : 0x%02x" % self.compression_type
         print "  > field_24           : 0x%x" % self.field_24
@@ -89,7 +174,7 @@ class BootloaderFooter(LittleEndianStructure):
 
     def dump(self):
         print "  > magic               : %s" % self.to_str(self.ilO_magic)
-        print "  > build_version       : %s" % self.build_version.split("\x1A")[0]
+        print "  > build_version       : %s" % self.build_version.split("\x1A")[0].strip()
         print "  > field_20            : 0x%x" % self.field_20
         print "  > field_24            : 0x%x" % self.field_24
         print "  > kernel offset       : 0x%x" % self.kernel_offset
@@ -123,7 +208,7 @@ class ImgHeader(LittleEndianStructure):
 
     def dump(self):
         print "  > magic              : %s" % self.to_str(self.ilO_magic)
-        print "  > build_version      : %s" % self.build_version.split("\x1A")[0]
+        print "  > build_version      : %s" % self.build_version.split("\x1A")[0].strip()
         print "  > type               : 0x%02x" % self.type
         print "  > compression_type   : 0x%02x" % self.compression_type
         print "  > field_24           : 0x%x" % self.field_24
@@ -138,22 +223,22 @@ class ImgHeader(LittleEndianStructure):
 
 
 # decompress extracted images
-def decompress_all(data,fname,chunks=0x10000):
-    global window,wchar
+def decompress_all(data, fname, chunks=0x10000):
+    global window, wchar
 
-    window=['\0']*0x1000
+    window = ['\0']*0x1000
     wchar = 0
 
     fff = open(fname, "wb")
 
     while len(data)>0:
-        ret = decompress(data[:chunks],fff)
+        ret = decompress(data[:chunks], fff)
         if len(data) < chunks:
             if ret == 0:
                 data = ""
             else:
                 data = data[-ret:]
-            ret = decompress(data[:chunks],fff,limit=0)
+            ret = decompress(data[:chunks], fff, limit=0)
             if ret == 0:
                 data = ""
             else:
@@ -166,12 +251,12 @@ def decompress_all(data,fname,chunks=0x10000):
 
 
 def decompress(data, fff, limit=16):
-    global window,wchar
-    out=""
+    global window, wchar
+    out = ""
 
     while len(data)>limit:
-        comp=ord(data[0])
-        data=data[1:]
+        comp = ord(data[0])
+        data = data[1:]
 
         for i in xrange(8):
             if limit == 0:
