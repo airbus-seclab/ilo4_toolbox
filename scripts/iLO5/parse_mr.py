87e0d49115057761903bad355d27782f700ef5d6
Alexandre Gazet <alexandre.gazet@airbus.com>
2021-08-02 13:47:51 +0200

    update iLO5 kernel helper scripts

--- scripts/iLO5/parse_mr.py
+++ scripts/iLO5/parse_mr.py
@@ -1,43 +1,56 @@
 import idc
 
+
+# for iLO5 1.20.33
+START_ADDR = 0x410CBC74
+
+# for iLO5 1.30.35
+START_ADDR = 0x410CCC74
+
+# for iLO5 1.37.06
+START_ADDR = 0x410CCC84
+
+# for iLO5 1.48.02
+START_ADDR = 0x410CCC84
+
+# for iLO5 2.33.16
+START_ADDR = 0x410CECD4
+
+# for ILO5 2.41.02
+START_ADDR = 0x410CECD4
+
+# for iLO5 2.44.17
+START_ADDR = 0x410CFCFC
+
+
 def dump_memory_region(ea):
+    idc.create_struct(ea, -1, "MEMORY_REGION")
 
-    idc.MakeStruct(ea,"MEMORY_REGION");
-
-    id = idc.Dword(ea)
-    flag1 = idc.Dword(ea+0x18)
-    flag2 = idc.Dword(ea+0x1C)
-    low = idc.Dword(ea+0x8)
-    high = idc.Dword(ea+0x10)
-    mask_low = idc.Dword(ea+0x20)
-    mask_high = idc.Dword(ea+0x28)
-    name = idc.Dword(ea+0x30)
-    memory_region = idc.GetString(name, -1, idc.ASCSTR_C)
-    print "    id: 0x%04x - flags 0x%x 0x%x - reg: %10s - low 0x%08x / high 0x%08x - mask 0x%08x / 0x%08x" % (id, flag1, flag2, memory_region, low, high, mask_low, mask_high)
+    mem_id = idc.get_wide_dword(ea)
+    flag1 = idc.get_wide_dword(ea+0x18)
+    flag2 = idc.get_wide_dword(ea+0x1C)
+    low = idc.get_wide_dword(ea+0x8)
+    high = idc.get_wide_dword(ea+0x10)
+    mask_low = idc.get_wide_dword(ea+0x20)
+    mask_high = idc.get_wide_dword(ea+0x28)
+    name = idc.get_wide_dword(ea+0x30)
+    memory_region = ida_bytes.get_strlit_contents(name, -1, idc.STRTYPE_C).decode()
+    print("    id: 0x%04x - flags 0x%x 0x%x - name: %10s - low 0x%08x / high 0x%08x - mask 0x%08x / 0x%08x" % (mem_id, flag1, flag2, memory_region, low, high, mask_low, mask_high))
 
     if memory_region.startswith("MR"):
         idc.set_name(ea, memory_region, idc.SN_PUBLIC)
     else:
         idc.set_name(ea, "MR_" + memory_region, idc.SN_PUBLIC)
-        
-# for iLO5 1.30.35
-START_ADDR = 0x410CCC74
-
-# for iLO5 1.20.33
-START_ADDR = 0x410CBC74
-
-# for iLO5 1.37.06
-START_ADDR = 0x410CCC84
 
 
 ea = START_ADDR
 mod_base = START_ADDR
 
-print "> parsing memory region entries:\n"
+print("> parsing memory region entries:\n")
 
 while True:
-    idc.MakeDword(ea)
-    struct_addr = idc.Dword(ea)
+    ida_bytes.create_data(ea, FF_DWORD, 4, ida_idaapi.BADADDR)
+    struct_addr = idc.get_wide_dword(ea)
     if struct_addr == 0:
         break
 
@@ -45,4 +58,4 @@ while True:
     dump_memory_region(struct_addr)
     ea += 4
 
-print "[+] job done captain!"
+print("[+] job done captain!")
--- scripts/iLO5/secinfo5.py
+++ scripts/iLO5/secinfo5.py
@@ -2,22 +2,38 @@ import idaapi
 import idc
 from ctypes import *
 
+
 # struct MAP_ENTRY
 # {
-  # MAP_ENTRY *next;
-  # int ptr_name;
-  # int base;
-  # int size;
-  # int access;
-  # int field_14;
+#     MAP_ENTRY *next;
+#     int ptr_name;
+#     int base;
+#     int size;
+#     int access;
+#     int field_14;
 # };
 
-# struct BSS_ENTRY
-# {
-#   void *ptr;
-#   int init_value;
-#   int size;
-# };
+
+# for iLO5 1.30.35
+SEC_INFO_LINKED_LIST = 0x410C580C
+
+# for iLO5 1.20.33
+SEC_INFO_LINKED_LIST = 0x410C480C
+
+# for ILO5 1.37.06
+SEC_INFO_LINKED_LIST = 0x410C580C
+
+# for ILO5 2.10
+SEC_INFO_LINKED_LIST = 0x410C680C
+
+# for ILO5 2.33.16
+SEC_INFO_LINKED_LIST = 0x410C780C
+
+# for ILO5 2.41.02
+SEC_INFO_LINKED_LIST = 0x410C780C
+
+# for ILO5 2.44.17
+SEC_INFO_LINKED_LIST = 0x410C880C
 
 
 class MapEntry(LittleEndianStructure):
@@ -32,17 +48,16 @@ class MapEntry(LittleEndianStructure):
     ]
 
     def to_str(self, ea):
-        max_len = idaapi.get_max_ascii_length(ea, ASCSTR_C, 0)
-        idaapi.make_ascii_string(ea, max_len, ASCSTR_C)
-        return idc.GetString(ea, max_len, ASCSTR_C)
+        max_len = idaapi.get_max_strlit_length(ea, idc.STRTYPE_C, 0)
+        idaapi.create_strlit(ea, max_len, idc.STRTYPE_C)
+        return ida_bytes.get_strlit_contents(ea, max_len, idc.STRTYPE_C).decode()
 
     def dump(self):
-        print "  %20s - base 0x%08x - size 0x%08x - access : 0x%x" % (
-            self.to_str(self.name_addr), self.base, self.size, self.access)
+        print("  %20s - base 0x%08x - size 0x%08x - access : 0x%x" % (
+            self.to_str(self.name_addr), self.base, self.size, self.access))
 
     def name(self):
         return "%s" % self.to_str(self.name_addr)
-
 
 
 class BssEntry(LittleEndianStructure):
@@ -54,48 +69,42 @@ class BssEntry(LittleEndianStructure):
     ]
 
     def dump(self, name=''):
-        print "  memory area at 0x%08x - size 0x%08x - init value 0x%02x - \"%s\"" % (
-            self.base, self.size,  self.init_value, name)
-
+        print("  memory area at 0x%08x - size 0x%08x - init value 0x%02x - \"%s\"" %
+              (self.base, self.size,  self.init_value, name))
 
 
 def make_segment(base, size, name):
-    s = idc.get_segm_by_sel(base)
-    if not s == 0xffffffff:
-        return
+    s = ida_segment.get_segm_num(base)
+    if s == -1:
 
-    s = idaapi.segment_t()
-    s.startEA = base
-    s.endEA = base+size
-    s.sel = 0
-    s.bitness = 1
-    s.comb = idaapi.scPub
-    idaapi.add_segm_ex(s, name.strip('.').upper(), "", idaapi.ADDSEG_NOSREG|idaapi.ADDSEG_SPARSE)
+        print("----")
+        print(hex(s))
+        print(hex(base))
+        print(hex(size))
+        print(name)
+        print("----")
 
-
-
-print "> parsing .secinfo entries:\n"
-
-sid  = GetStrucIdByName('MAP_ENTRY')
-ssize = idc.GetStrucSize(sid)
-
-# for iLO5 1.30.35
-SEC_INFO_LINKED_LIST = 0x410C580C
-
-# for iLO5 1.20.33
-SEC_INFO_LINKED_LIST = 0x410C480C
-
-# for ILO5 1.37.06
-SEC_INFO_LINKED_LIST = 0x410C580C
+        s = idaapi.segment_t()
+        s.start_ea = base
+        s.end_ea = base+size
+        s.sel = 0
+        s.bitness = 1
+        s.comb = idaapi.scPub
+        idaapi.add_segm_ex(s, name.strip('.').upper(), "", idaapi.ADDSEG_NOSREG | idaapi.ADDSEG_SPARSE)
 
 
+print("> parsing .secinfo entries:\n")
+
+sid = ida_struct.get_struc_id('MAP_ENTRY')
+ssize = ida_struct.get_struc_size(sid)
 ea = SEC_INFO_LINKED_LIST
+
 entries = []
 
 while True:
-    idc.MakeUnknown(ea, ssize, idc.DOUNK_DELNAMES)
-    idaapi.doStruct(ea, ssize, sid);
-    buf = idc.GetManyBytes(ea, ssize)
+    ida_bytes.del_items(ea, ssize, DELIT_DELNAMES)
+    ida_bytes.create_struct(ea, ssize, sid)
+    buf = idc.get_bytes(ea, ssize)
 
     entry = MapEntry.from_buffer_copy(buf)
     entries.append(entry)
@@ -107,30 +116,30 @@ while True:
     ea = entry.next
 
 
-print "\n> setting-up segments:\n"
+print("\n> setting-up segments:\n")
 
 for entry in [e for e in entries if e.size != 0]:
-    make_segment(entry.base, entry.size, entry.name())
-
+    if entry.size != 0:
+        make_segment(entry.base, entry.size, entry.name())
 
 
 bss_list = [e for e in entries if e.name() == '.secinfo']
 
 if len(bss_list) == 1:
-    print "\n> parsing BSS entries:\n"
+    print("\n> parsing BSS entries:\n")
 
     secinfo = bss_list[0]
     ea = secinfo.base
-    sid  = GetStrucIdByName('BSS_ENTRY')
-    ssize = idc.GetStrucSize(sid)
+    sid = ida_struct.get_struc_id('BSS_ENTRY')
+    ssize = ida_struct.get_struc_size(sid)
 
     while True:
-        idc.MakeUnknown(ea, ssize, idc.DOUNK_DELNAMES)
-        idaapi.doStruct(ea, ssize, sid);
-        buf = idc.GetManyBytes(ea, ssize)
+        ida_bytes.del_items(ea, ssize, DELIT_DELNAMES)
+        ida_bytes.create_struct(ea, ssize, sid)
+        buf = idc.get_bytes(ea, ssize)
         bsse = BssEntry.from_buffer_copy(buf)
 
-        elist =  [e for e in entries if (e.base == bsse.base and e.size != 0)]
+        elist = [e for e in entries if (e.base == bsse.base and e.size != 0)]
         if len(elist) == 1:
             bsse.dump(elist[0].name())
         else:
@@ -141,4 +150,4 @@ if len(bss_list) == 1:
             break
 
 
-print "\n[+] job done captain!\n"
+print("\n[+] job done captain!\n")
